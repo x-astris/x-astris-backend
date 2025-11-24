bdd6b2bc0b44356c326b4ae066be1cb643007f83
x-astris <info@x-astris.com>
2025-11-24 16:01:41 +0100

    Fix Render port + CORS for Vercel

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // frontend
+    origin: [
+      'http://localhost:3000',
+      'https://x-astris-frontend.vercel.app'
+    ],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
 
-  await app.listen(3333);
-  console.log("RUNNING ON 3333 with CORS enabled");
+  await app.listen(process.env.PORT || 3333);
+  console.log("RUNNING ON PORT:", process.env.PORT || 3333);
 }
 
 bootstrap();
-
